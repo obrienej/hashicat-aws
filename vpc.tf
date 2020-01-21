f14d5de2f93b631023379f46d06c9256bc5c8b96
obrienej <obrien.ericj@gmail.com>
2020-01-21 15:24:57 -0500

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -2,11 +2,9 @@ module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "2.23.0"
   # insert the 8 required variables here
-}
 resource "aws_vpc" "main" {
   cidr_block       = "10.0.0.0/16"
   instance_tenancy = "dedicated"
-
   tags = {
     Name = "main"
   }
